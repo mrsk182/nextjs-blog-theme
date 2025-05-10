1582505c4e9b8ea3dff735a08f894ae39d7d80ee
mrsk182 <senaakrty@gmail.com>
2025-05-10 10:53:08 +0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'mraven';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'xTeknoloji';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Tüm Hakları Saklıdır.2025©';
 
   return {
     name,
